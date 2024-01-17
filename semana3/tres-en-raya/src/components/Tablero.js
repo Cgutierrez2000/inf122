e3ec971f2ae2e24b45636117632c0a8920ba92c7
Cgutierrez2000 <carlixalex@gmail.com>
2024-01-16 20:55:20 -0400

    commit

--- semana3/tres-en-raya/src/components/Tablero.js
+++ semana3/tres-en-raya/src/components/Tablero.js
@@ -2,30 +2,48 @@ import React from "react";
 import Cuadro from "./Cuadro";
 import '../style/Tablero.css';
 import { useState } from "react";
-function Tablero({cuadros, onClick}){
-    function renderizarCuadro(i) {
-    return (
-        <Cuadro
-            valor={cuadros[i]}
-            funcion={() => onClick(i)}
-        />
-    );
-}
+
+function Tablero({cuadros, onClick}) {
+    const [cuadros, setCuadros] = useState(Array(9).fill(null));
+    const [jugador, setJugador] = useState("O");
+
+    const click = (i) => {
+        if (cuadros[i]) return;
+        const cuadrosTemp = [...cuadros];
+        cuadrosTemp[i] = jugador;
+        setCuadros(cuadrosTemp);
+        if (jugador === "X") {
+            setJugador("O");
+        } else {
+            setJugador("X");
+        }
+    }
+
+    const renderizarCuadro = (i) => {
+        return (
+            <Cuadro
+                valor={cuadros[i]}
+                funcion={() => click(i)}
+            />
+        );
+    }
+
     return (
         <div className='juego'>
             <h1>Siguiente Jugador: {jugador}</h1>
             <div className="tablero">
-                <Cuadro valor={renderizarCuadro(0)} />
-                <Cuadro valor={renderizarCuadro(1)} />
-                <Cuadro valor={renderizarCuadro(2)} />
-                <Cuadro valor={renderizarCuadro(3)} />
-                <Cuadro valor={renderizarCuadro(4)} />
-                <Cuadro valor={renderizarCuadro(5)} />
-                <Cuadro valor={renderizarCuadro(6)} />
-                <Cuadro valor={renderizarCuadro(7)} />
-                <Cuadro valor={renderizarCuadro(8)} />
+                {renderizarCuadro(0)}
+                {renderizarCuadro(1)}
+                {renderizarCuadro(2)}
+                {renderizarCuadro(3)}
+                {renderizarCuadro(4)}
+                {renderizarCuadro(5)}
+                {renderizarCuadro(6)}
+                {renderizarCuadro(7)}
+                {renderizarCuadro(8)}
             </div>
         </div>
     );
 }
+
 export default Tablero;
